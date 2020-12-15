c7005a71aba0b2831e073e1c74b402fc85b28eff
Kirill Fesiuk <kirill167349@gmail.com>
2020-12-15 09:57:27 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import datetime
+from typing import Callable
+
+def logger_factory(file):
+    def logger(func=Callable):
+        def new_func(*args, **kwargs):
+            with open(file, 'w+', encoding='utf-8') as f:
+                f.write(f'Дата и время: {datetime.datetime.now()}\n')
+                f.write(f'Название функции: {func.__name__}\n')
+                f.write(f'Аргументы функции: {args} и {kwargs}\n')
+                f.write(f'Возращаемое значение: {func(*args, **kwargs)}\n')
+
+        return new_func
+    return logger
+
+@logger_factory('result1.txt')
+def my_generator(file):
+    with open(file , "rb") as f:
+        content = f.readlines()
+        for line in content:
+            try:
+                hash_object = hashlib.md5(line)
+                yield hash_object.hexdigest()
+
+            except:
+                yield "Error"
+
+if __name__ == '__main__':
+    print(my_generator('result1.txt'))
