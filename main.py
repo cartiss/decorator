defd74f7214a13dc950edbb275243ce55da10c8e
Kirill Fesiuk <kirill167349@gmail.com>
2020-12-26 23:14:15 +0200

    Corrects return 'new_func'

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def logger_factory(file):
                 f.write(f'Название функции: {func.__name__}\n')
                 f.write(f'Аргументы функции: {args} и {kwargs}\n')
                 f.write(f'Возращаемое значение: {func(*args, **kwargs)}\n')
-
+            return 'new_func - Done!'
         return new_func
     return logger
 
